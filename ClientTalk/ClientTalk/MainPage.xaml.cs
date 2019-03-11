854449a3bc9a858476c4cdccf886ae3e8eb844a1
DangNguyenTH4 <dangnt520@wru.vn>
2019-03-11 19:43:46 +0700

    Talk wit someone, realtime, Use SignalR, Server,Android

---
+++ ClientTalk/ClientTalk/MainPage.xaml.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ClientTalk
+{
+    public class Message
+    {
+        public string Content { get; set; }
+        public bool IsMine { get; set; }
+    }
+    public partial class MainPage : ContentPage
+    {
+        IHubProxy ChatHub { get; }
+        HubConnection HubConnect{ get; }
+
+        public ObservableCollection<Message> CollectionMessage { get; set; }
+        ////5.1
+        //string URLServer = "http://192.168.56.1:5000";
+        //7.0
+        string URLServer = "http://192.168.114.2:5000";
+        public MainPage()
+        {
+            InitializeComponent();
+            CollectionMessage = new ObservableCollection<Message>();
+            HubConnect = new HubConnection(URLServer);
+            ChatHub = HubConnect.CreateHubProxy("ChatHub");
+            ChatHub.On("Ready", OnReady);
+            ChatHub.On<string, string>("Message", OnMessage);
+            MainListView.ItemsSource = CollectionMessage;
+        }
+
+        public bool IsReady { get; set; } = false;
+        private  void OnMessage(string mes, string userID)
+        {
+            if (mes.Equals(""))
+            {
+                OutRoomVoid();
+                //If don't have this line, user Second will not be Stop HubConnect,
+                //But if have this line, Will be disconnected, before done line 104 (await ChatHub.Invoke(chat..)
+                if(!IGetOut)
+                {
+                    if (HubConnect.State == ConnectionState.Connected)
+                        HubConnect.Stop();
+                }
+
+            }
+            else
+            {
+                Device.BeginInvokeOnMainThread(() => {
+                    var msg = new Message() { Content = mes, IsMine = userID == HubConnect.ConnectionId };
+                    CollectionMessage.Add(msg);
+                });
+
+            }
+        }
+        private void OnReady()
+        {
+            IsReady = true;
+            UpdateStatus("Đã đủ người rồi nói chuyện thôi");
+
+        }
+
+        #region Element/View
+        #endregion
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            await HubConnect.Start();
+            GetInARoomVoid();
+            if (!IsReady)
+                UpdateStatus("Đã vào phòng");
+        }
+        private void UpdateStatus(string message)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Statuss.Text = message;
+            });
+        }
+        private void OnTwoPersionInRoom()
+        {
+            UpdateStatus("Đã đủ người rồi nói chuyện thôi");
+            Thread.Sleep(3000);
+            UpdateStatus("Rời phòng");
+        }
+        private async void btn_onSendClicked(object sender, EventArgs e)
+        {
+            if (ChatEntry.Text.Equals("") || !IsReady)
+            {
+                return;
+            }
+            SendButton.IsEnabled = false;
+            await ChatHub.Invoke("Chat", ChatEntry.Text);
+            ChatEntry.Text = string.Empty;
+            SendButton.IsEnabled = true;
+        }
+
+        bool IGetOut = false;
+        private  async void btn_onGetOutClicked(object sender, EventArgs e)
+        {
+            IGetOut = true;
+            OutRoom.IsEnabled = false;
+            try
+            {
+                await ChatHub.Invoke("Chat", "");
+            }
+            catch { }
+            //Wait for get Message "GetOut", then Stop Connect the HubConnect.
+            //If, HubConnect.Stop() in line 130, Will be crash, because the asyncho, (stop Hub, still...)
+            if (HubConnect.State==ConnectionState.Connected)
+                HubConnect.Stop();
+        }
+        private void GetInARoomVoid()
+        {
+            IGetOut = false;
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                MainButton.IsEnabled = false;
+                OutRoom.IsEnabled = true;
+                SendButton.IsEnabled = true;
+            });
+        }
+        private void OutRoomVoid()
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                MainButton.IsEnabled = true;
+                OutRoom.IsEnabled = false;
+                SendButton.IsEnabled = false;
+                IsReady = false;
+                CollectionMessage.Clear();
+                UpdateStatus("......");
+            });
+            //HubConnect.Stop();
+        }
+    }
+}
---
+++ ServerTalk/ChatHub.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerTalk
+{
+    public class ChatHub : Hub
+    {
+        public static List<Room> Rooms { get; set; } = new List<Room>();
+
+        public override async Task OnConnected()
+        {
+            await base.OnConnected();
+            //Use Linq to search Room, that not enough Persion (Max 2)
+            var room = Rooms.FirstOrDefault(r => r.Users.Count < 2);
+            if (room != null)
+            {
+                //Each user connect to Hub, Have only one ConectionID
+                room.Users.Add(Context.ConnectionId);
+                if (room.Users.Count == 2)
+                {
+                    //Enough 2 persion to talk, send Ready Message to 2client
+                    Clients.Clients(room.Users).Ready();
+                }
+            }
+            else
+            {
+                room = new Room();
+                room.Users.Add(Context.ConnectionId);
+                Rooms.Add(room);
+            }
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+
+        //When Recieve A Request Out Room
+        public void OutOfRoom()
+        {
+            var room = Rooms.FirstOrDefault(x => x.Users.Contains(Context.ConnectionId));
+            if (room != null)
+            {
+                //Send to all client message Out room
+                Clients.Clients(room.Users).Message("OutRoom");
+                Rooms.Remove(room);
+            }
+        }
+        //When Recieve A Message
+        public void Chat(string mes)
+        {
+            
+            var room = Rooms.FirstOrDefault(x => x.Users.Contains(Context.ConnectionId));
+            if (room != null)
+            {
+                //Send to all (2) client a message content : message content, and UserID of Sender.
+                Clients.Clients(room.Users).Message(mes, Context.ConnectionId);
+                if (mes.Equals(""))
+                    Rooms.Remove(room);
+            }
+        }
+    }
+    public class Room
+    {
+        public string Id { get; set; }
+        public List<string> Users { get; set; }
+        public Room()
+        {
+            Id = Guid.NewGuid().ToString();
+            Users = new List<string>();
+        }
+    }
+}
